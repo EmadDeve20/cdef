e61f3eb6e3c164ecb6dd7a9ae51d9d66711ad8c5
ED <emaddeve20@gmail.com>
2021-02-22 23:36:37 +0330

    adding String class and strstr def for first step

--- cdef/cdef.py
+++ cdef/cdef.py
@@ -1,2 +1,17 @@
 #!/bin/env python3
 
+class String:
+    """this is class about string like <string.h> library in the C"""
+    def strstr(text: str, target: str):
+        result = ""
+        find_index = 0
+        for i in range(len(text)):
+            if text[i:i + len(target)] == target:
+                find_index = i
+                break
+        for i in range(find_index, len(text)):
+            if text[i] != " ":
+                result += text[i]
+            else:
+                break
+        return result
