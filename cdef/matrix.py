ede046bd8f15f571c60529ef92e19d12c2774f17
EmadDeve20 <EmadDeve20@gmail.com>
2021-07-07 13:53:17 +0430

    removing usles function

--- cdef/matrix.py
+++ cdef/matrix.py
@@ -29,10 +29,6 @@ def get_value_with_index(arr: List, index: int):
             index -= len(arr)
         return arr[index]
 
-def get_index_with_value(arr: List, value):
-    for i in range(len(arr)):
-        if arr[i] == value:
-            return i
 
 def get_indexs_with_values(arr: List, values: List) -> list:
     res = []
